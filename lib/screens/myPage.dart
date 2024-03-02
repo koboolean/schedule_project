e666855f990e2246808a179db6e548354a28081a
조현준 <45157196+koboolean@users.noreply.github.com>
2024-03-02 21:43:26 +0900

    마이페이지 관련 수정사항 추가

--- lib/screens/myPage.dart
+++ lib/screens/myPage.dart
@@ -36,240 +36,285 @@ class _MyPageState extends State<MyPage> {
     return Consumer(
       builder: (context, bucketService, child) {
         return Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 18.0),
-                child: Text(
-                  "마이페이지",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 24,
-                  ),
+            body: SingleChildScrollView(
+              child :Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
                 ),
-              ),
-              const SizedBox(
-                height: 31,
-              ),
-              Column(
-                children: [
-                  Center(
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: const Icon(
-                          Icons.account_circle,
-                          size: 158,
-                          color: Colors.grey,
-                        )),
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    '${user.email}',
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: Color.fromRGBO(128, 128, 128, 1),
+                const Padding(
+                  padding: EdgeInsets.only(left: 18.0),
+                  child: Text(
+                    "마이페이지",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24,
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 26,
-              ),
-              Container(
-                height: 7,
-                decoration: const BoxDecoration(
-                  color: Color(0xfff3f3f3),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 18.0),
-                child: Column(
+                const SizedBox(
+                  height: 31,
+                ),
+                Column(
                   children: [
+                    Center(
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: const Icon(
+                            Icons.account_circle,
+                            size: 158,
+                            color: Colors.grey,
+                          )),
+                    ),
+                    const SizedBox(height: 15),
                     Text(
-                      "앱 설정",
-                      style:
-                      TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      '${user.email}',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Color.fromRGBO(128, 128, 128, 1),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 23,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 30.0, bottom: 10),
-                child: Row(
-                  children: [
-                    const Text(
-                      "앱 버전",
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    const Spacer(),
-                    const SizedBox(width: 200),
-                    ValueListenableBuilder(
-                      valueListenable: version,
-                      builder:
-                          (BuildContext context, String value, Widget? child) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(version.value,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                )),
-                          ],
-                        );
-                      },
-                    ),
-                  ],
+                const SizedBox(
+                  height: 26,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-                child: Row(children: [
-                  const Text("암호설정", style : TextStyle(fontSize: 16)),
-                  const Spacer(),
-                  IconButton(
-                    icon: Icon(
-                      oAuthList.degree2Yn ? Icons.check_box_outlined : Icons.check_box_outline_blank,
-                      color: Colors.black),
-                    onPressed: () async {
-                      /* 팝업형식에서 화면형식으로 변경
-                      oAuthList.degree2Yn ? await passwordDialog(context, "d", deleteDegree2Password, (returnVal){
-                        if(returnVal){
-                          setState(() {
-                            oAuthList.degree2Yn = !authService.oAuthList[0].degree2Yn;
-                          });
-                          LocalAuthApi().setLcalAuthSetYn(false);
-                        }
-                      }) : await passwordDialog(context, "i", saveDegree2Password, (returnVal){
-                        if(returnVal){
-                          setState(() {
-                            oAuthList.degree2Yn = !authService.oAuthList[0].degree2Yn;
-                          });
-                        }
-                      });
-                       */
-                      String degree2Yn = oAuthList.degree2Yn ? "d" : "i";
-
-                      var isYn = await Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => PasswordPage(degree2Yn : degree2Yn)),
-                      );
-
-                      if(isYn == true){
-                        setState(() {
-                          oAuthList.degree2Yn = !authService.oAuthList[0].degree2Yn;
-                        });
-                      }
-                    },
-                  )
-                ],),
-              ),
-              (oAuthList.degree2Yn && LocalAuthApi.localAuthYn(context) == true) ? Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-                child: Row(
-                  children: [
-                    const Text("생체인증 사용하기",
-                      style: TextStyle(
-                        fontSize: 16,
+                Container(
+                  height: 7,
+                  decoration: const BoxDecoration(
+                    color: Color(0xfff3f3f3),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 18.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "앱 설정",
+                        style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                       ),
-                    ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 23,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 30.0, bottom: 10),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "앱 버전",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const Spacer(),
+                      const SizedBox(width: 200),
+                      ValueListenableBuilder(
+                        valueListenable: version,
+                        builder:
+                            (BuildContext context, String value, Widget? child) {
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text(version.value,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  )),
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 23,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 18.0, right: 30.0, bottom: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        "알람설정",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 23,
+                ),
+                Container(
+                  height: 7,
+                  decoration: const BoxDecoration(
+                    color: Color(0xfff3f3f3),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 18.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "계정설정",
+                        style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 23,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                  child: Row(children: [
+                    const Text("암호설정", style : TextStyle(fontSize: 16)),
                     const Spacer(),
                     IconButton(
                       icon: Icon(
-                          LocalAuthApi.isLcalAuthSetYn ? Icons.check_box_outlined : Icons.check_box_outline_blank,
-                          color: Colors.black),
+                        oAuthList.degree2Yn ? Icons.check_box_outlined : Icons.check_box_outline_blank,
+                        color: Colors.black),
                       onPressed: () async {
-                        var isCeck = await LocalAuthApi.authenticate();
+                        /* 팝업형식에서 화면형식으로 변경
+                        oAuthList.degree2Yn ? await passwordDialog(context, "d", deleteDegree2Password, (returnVal){
+                          if(returnVal){
+                            setState(() {
+                              oAuthList.degree2Yn = !authService.oAuthList[0].degree2Yn;
+                            });
+                            LocalAuthApi().setLcalAuthSetYn(false);
+                          }
+                        }) : await passwordDialog(context, "i", saveDegree2Password, (returnVal){
+                          if(returnVal){
+                            setState(() {
+                              oAuthList.degree2Yn = !authService.oAuthList[0].degree2Yn;
+                            });
+                          }
+                        });
+                         */
+                        String degree2Yn = oAuthList.degree2Yn ? "d" : "i";
 
-                        if(isCeck){
+                        var isYn = await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => PasswordPage(degree2Yn : degree2Yn)),
+                        );
+
+                        if(isYn == true){
                           setState(() {
-                            LocalAuthApi().setLcalAuthSetYn(!LocalAuthApi.isLcalAuthSetYn);
+                            oAuthList.degree2Yn = !authService.oAuthList[0].degree2Yn;
                           });
-
                         }
                       },
                     )
-                  ],
+                  ],),
                 ),
-              ) : Row(),
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      "로그아웃",
-                      style: TextStyle(
-                        fontSize: 16,
+                (oAuthList.degree2Yn && LocalAuthApi.localAuthYn(context) == true) ? Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                  child: Row(
+                    children: [
+                      const Text("생체인증 사용하기",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.black,
+                      const Spacer(),
+                      IconButton(
+                        icon: Icon(
+                            LocalAuthApi.isLcalAuthSetYn ? Icons.check_box_outlined : Icons.check_box_outline_blank,
+                            color: Colors.black),
+                        onPressed: () async {
+                          var isCeck = await LocalAuthApi.authenticate();
+
+                          if(isCeck){
+                            setState(() {
+                              LocalAuthApi().setLcalAuthSetYn(!LocalAuthApi.isLcalAuthSetYn);
+                            });
+
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                ) : Row(),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "로그아웃",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
                       ),
-                      onPressed: () async {
-                        // 로그아웃
-                        bool? result = await showConfirmationDialog(
-                          context,
-                          '로그아웃',
-                          '로그아웃하시겠습니까?',
-                        );
-
-                        if(result == true){
-                          context.read<AuthService>().signOut();
-
-                          // 로그인 페이지로 이동
-                          Navigator.pushReplacement(
+                      const Spacer(),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.black,
+                        ),
+                        onPressed: () async {
+                          // 로그아웃
+                          bool? result = await showConfirmationDialog(
                             context,
-                            MaterialPageRoute(builder: (context) => const LoginPage()),
+                            '로그아웃',
+                            '로그아웃하시겠습니까?',
                           );
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      "탈퇴하기",
-                      style: TextStyle(
-                        fontSize: 16,
+
+                          if(result == true){
+                            context.read<AuthService>().signOut();
+
+                            // 로그인 페이지로 이동
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const LoginPage()),
+                            );
+                          }
+                        },
                       ),
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.black,
-                      ),
-                      onPressed: () async {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const DeleteUserPage()),
-                        );
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "탈퇴하기",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.black,
+                        ),
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const DeleteUserPage()),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         );
       },
